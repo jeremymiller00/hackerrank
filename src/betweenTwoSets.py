f4f35ed49d0a0fb4f9eab3eadf80dadb0720ada8
Jeremy Miller <jeremymiller00@gmail.com>
2020-05-04 16:50:06 -0700

    finish problem

---
+++ src/betweenTwoSets.py
@@ -0,0 +1,17 @@
+from functools import reduce
+
+def getTotalX(a, b):
+
+    def gcd(a, b):
+        while a % b != 0:
+            a, b = b, (a % b)
+        return b
+    
+    def lcm(a, b):
+        return a * b // gcd(a, b)
+
+    min_gcd = reduce(gcd, b)
+    max_lcm = reduce(lcm, a)
+    count = sum([1 for x in range(max_lcm, min_gcd+1, max_lcm) if min_gcd % x == 0])
+ 
+    return count
